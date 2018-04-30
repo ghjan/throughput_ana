49bcce13c739febb62cf798b3a7f77bd72dd41d5
David Zhang <cajan2@163.com>
2018-04-30 23:40:10 +0800

    add lamp

---
+++ lamp/lamp_run.sh
@@ -0,0 +1,5 @@
+docker run -p 80:80 -p 3336:3306 
+ -v /data/lamp/www:/var/www \
+ -v /data/lamp/apache-conf/apache2.conf:/etc/apache2/apache2.conf \
+ -v /data/lamp/mysql-conf/my.cnf:/etc/mysql/my.cnf \
+ -t -i linode/lamp /bin/bash
