70e76b70e267cea7ec130afda2a7cf3079c9892e
David Zhang <cajan2@163.com>
2018-05-01 16:16:20 +0800

    modify lamp_init.sh

--- lamp/lamp_init.sh
+++ lamp/lamp_init.sh
@@ -1,2 +1,2 @@
 #lamp_init.sh
-docker run -p 80:80 -p 3336:3306 -v /data/lamp/www:/var/www -v -t -i linode/lamp /bin/bash
+docker run -p 80:80 -p 3336:3306 -v /data/lamp/www:/var/www -t -i linode/lamp /bin/bash
