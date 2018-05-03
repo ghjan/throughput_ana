7d662fce0b854fe8fb496e6a375b36190b79af9e
jian zhang <jian.zhang@fuwo.com>
2018-05-03 14:41:39 +0800

    save

--- analysis/tryredis/tryredis.go
+++ analysis/tryredis/tryredis.go
@@ -7,13 +7,14 @@ import (
 	"github.com/mediocregopher/radix.v2/redis"
 )
 
-const ()
+const (
+	REDIS_ADDRESS = "localhost:6379"
+)
 
-func main() {
-	// Establish a connection to the Redis server listening on port 6379 of the
-	// local machine. 6379 is the default port, so unless you've already
-	// changed the Redis configuration file this should work.
-	conn, err := redis.Dial("tcp", "localhost:6379")
+var rredisCli redis.Client
+
+func init() {
+	conn, err := redis.Dial("tcp", REDIS_ADDRESS)
 	if err != nil {
 		log.Fatal(err)
 	}
@@ -21,20 +22,56 @@ func main() {
 	// closed before exiting the main() function.
 	defer conn.Close()
 
+}
+func main() {
+	hmset()
+	hmget()
+
+	hget()
+}
+func hget() {
+	// Issue a HGET command to retrieve the title for a specific album, and use
+	// the Str() helper method to convert the reply to a string.
+	title, err := rredisCli.Cmd("HGET", "album:1", "title").Str()
+	if err != nil {
+		log.Fatal(err)
+	}
+	// Similarly, get the artist and convert it to a string.
+	artist, err := rredisCli.Cmd("HGET", "album:1", "artist").Str()
+	if err != nil {
+		log.Fatal(err)
+	}
+	// And the price as a float64...
+	price, err := rredisCli.Cmd("HGET", "album:1", "price").Float64()
+	if err != nil {
+		log.Fatal(err)
+	}
+	// And the number of likes as an integer.
+	likes, err := rredisCli.Cmd("HGET", "album:1", "likes").Int()
+	if err != nil {
+		log.Fatal(err)
+	}
+	fmt.Printf("%s by %s: £%.2f [%d likes]\n", title, artist, price, likes)
+}
+func hmget() {
+	resp := rredisCli.Cmd("HMGET", "album:1", "title", "artist", "price", "likes")
+	fmt.Println(resp.String())
+}
+func hmset() {
+	// Establish a connection to the Redis server listening on port 6379 of the
+	// local machine. 6379 is the default port, so unless you've already
+	// changed the Redis configuration file this should work.
 	// Send our command across the connection. The first parameter to Cmd()
 	// is always the name of the Redis command (in this example HMSET),
 	// optionally followed by any necessary arguments (in this example the
 	// key, followed by the various hash fields and values).
 	//HMSET key field1 value1 field2 value2
 	//127.0.0.1:6379> HMSET album:1 title "Electric Ladyland" artist "Jimi Hendrix" price 4.95 likes 8
-	resp := conn.Cmd("HMSET", "album:1", "title", "Electric Ladyland", "artist", "Jimi Hendrix", "price", 4.95, "likes", 8)
+	resp := rredisCli.Cmd("HMSET", "album:1", "title", "Electric Ladyland", "artist", "Jimi Hendrix", "price", 4.95, "likes", 8)
 	// Check the Err field of the *Resp object for any errors.
 	if resp.Err != nil {
 		log.Fatal(resp.Err)
 	}
-
-	fmt.Println(resp.Int())
-	fmt.Println("Electric Ladyland added!")
-	resp = conn.Cmd("HMGET", "album:1", "title", "artist", "price", "likes")
 	fmt.Println(resp.String())
+	fmt.Println("Electric Ladyland added!")
 }
